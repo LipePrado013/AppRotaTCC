95f95d7cd621a17cb36f264811ef8c2f054e7e0f
LipePrado013 <lipe.praado@gmail.com>
2023-09-26 15:52:37 -0300

    criação de mais locais na Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import { FlatList, Image, ScrollView, Text, TouchableOpacity, View, StyleSheet } from "react-native"
 
-import img from '../assets/img/museupesca.png'
-import img1 from '../assets/img/museu-do-cafe.png'
-import img2 from '../assets/img/Aquario.png'
+import museupesca from '../assets/img/museupesca.png'
+import museucafe from '../assets/img/museu-do-cafe.png'
+import aquario from '../assets/img/Aquario.png'
+import quebramar from '../assets/img/quebraMar.png'
 
 export const cards2 = [
     {// deacricao limite de 128 caracteres
         id: 1,
-        image: img,
+        image: museupesca,
         titulo: 'Museu da pesca',
         previa: 'Instalado em um casarão de 1908, o  Museu de Pesca, é uma das principais atrações turísticas de Santos.',
         data: 'Inaugurado em 1932',
@@ -18,7 +19,7 @@ export const cards2 = [
     },
     {
         id: 2,
-        image: img1,
+        image: museucafe,
         titulo: 'Museu do café',
         previa: 'Um lugar que reúne tradição, arquitetura, história, sabores e aromas. Instalado em um prédio de estilo eclético.',
         data: 'Inaugurado em 1998',
@@ -33,17 +34,18 @@ export const cards2 = [
 const cards = [
     {
         id: 1,
-        image: img,
+        image: museupesca,
         titulo: 'museu do da pesca',
         previa: 'Instalado em um casarão de 1908, o  Museu de Pesca, é uma das principais atrações turísticas de Santos.',
         descricao: 'Instalado em um casarão de 1908, o  Museu de Pesca, é uma das principais atrações turísticas de Santos e integra o patrimônio histórico-cultural de toda a região. De estilo eclético, o prédio, construído no local do antigo Forte Augusto (século XVIII), abriga importante acervo do ambiente aquático. Dentre as atrações, um esqueleto de baleia com 23m de comprimento, lulas gigantes, Sala da Praia, Ala Lúdica, animais taxidermizados e uma grande coleção de areias de praias do Brasil e do mundo.',
         data: 'inaugurado em 1932',
         url: 'https://www.google.com.br/maps/place/Museu+de+Pesca/@-23.9902073,-46.3090641,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce026c9975d8f3:0xb41a39d51a00f50b!8m2!3d-23.9902122!4d-46.3064892!16s%2Fg%2F11b66hhjxk?entry=ttu',
+        // type: 1
 
     },
     {
         id: 2,
-        image: img1,
+        image: museucafe,
         titulo: 'museu do café',
         previa: 'Um lugar que reúne tradição, arquitetura, história, sabores e aromas. Instalado em um prédio de estilo eclético.',
         descricao: 'Um lugar que reúne tradição, arquitetura, história, sabores e aromas. Instalado em um prédio de estilo eclético, com 6 mil m² e mais de 200 portas e janelas, o Museu do Café, inaugurado em 1998, é muito mais do que um local turístico que exalta o principal produto brasileiro de exportação no final do século 19. É uma experiência de variadas sensações, que vão do início do cultivo do grão até a consolidação do café como um dos símbolos nacionais. Exposições permanente e temporárias, obras de arte, mobiliário de época, loja temática e cafeteria que serve os melhores grãos café - e até o mais caro e raro do país - são algumas de suas muitas atrações.',
@@ -53,7 +55,7 @@ const cards = [
     },
     {
         id: 3,
-        image: img2,
+        image: aquario,
         titulo: 'Aquario',
         previa: 'Há sete décadas encantando gerações, o Aquário de Santos é o mais antigo do Brasil e desde 1995 consta do Guinness World Record.',
         descricao: 'Há sete décadas encantando gerações, o Aquário de Santos é o mais antigo do Brasil e desde 1995 consta do Guinness World Record. Espaço privilegiado de lazer e conhecimento, o parque é pioneiro em projetos de preservação do mar e de seus habitantes - foi a  primeira instituição brasileira a realizar resgate e recuperação dos animais marinhos.O Aquário ocupa uma área de 3.000 m², 2.214 dos quais abertos aos visitantes.',
@@ -62,12 +64,12 @@ const cards = [
     },
     {
         id: 4,
-        image: img2,
+        image: quebramar,
         titulo: 'Quabre Mar',
-        previa: 'Há sete décadas encantando gerações, o Aquário de Santos é o mais antigo do Brasil e desde 1995 consta do Guinness World Record.',
-        descricao: 'Com 42.766 m², o Parque Roberto Mário Santini ocupa a plataforma do emissário submarino e avança 400m mar adentro. É um espaço privilegiado de observação do mar e da orla, local perfeito para apreciar o pôr do sol.',
-        data: 'inaugurado em 1998',
-        url: 'https://www.google.com.br/maps/place/Aqu%C3%A1rio+Municipal+de+Santos/@-23.9862442,-46.3098878,17z/data=!4m10!1m2!2m1!1saquario!3m6!1s0x94ce024548f647a9:0x31768f2da5ccf9f0!8m2!3d-23.9862442!4d-46.3083714!15sCgdhcXVhcmlvWgkiB2FxdWFyaW-SAQhhcXVhcml1bZoBI0NoWkRTVWhOTUc5blMwVkpRMEZuU1VObGIyVklWbHBCRUFF4AEA!16s%2Fg%2F11r_phgnz?entry=ttu',
+        previa: 'Com 42.766 m², o Parque Roberto Mário Santini ocupa a plataforma do emissário submarino e avança 400m mar adentro.',
+        descricao: 'No parque, estão instalados Espaço Criança, pista profissional de skate com 1.100 m², ciclovia, mesas ao ar livre para jogos, gibiteca, arquibancada com capacidade para 600 pessoas junto ao quebra-mar, torre de observação para jurados em competições de surf, e áreas de convivência e de contemplação.',
+        data: 'inaugurado em 2009',
+        url: 'https://www.google.com.br/maps/place/Quebra+Mar/@-23.9699289,-46.3501784,17z/data=!4m6!3m5!1s0x94ce03fd03d98457:0x59e79b4431a852b!8m2!3d-23.9699289!4d-46.3501784!16s%2Fg%2F11s90r94hd?entry=ttu',
     },
 ]
 
@@ -76,11 +78,15 @@ export default function Home({ navigation }) {
 
     return (
         <>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={styles.titles}>
-                    <Text style={styles.TitleDestaque}> Recomendados </Text>
+            <ScrollView showsVerticalScrollIndicator={false} >
+                <View>
+                    <Text style={{
+                        fontSize: 25,
+                        fontWeight: 'bold',
+                        padding: 10,
+                    }}> Recomendados </Text>
                 </View>
-                <View style={styles.container}>
+                <View style={styles.container}>{/*container */}
                     <View style={styles.conteudo}>
 
                         <FlatList data={cards2} renderItem={({ item }) =>
@@ -99,13 +105,25 @@ export default function Home({ navigation }) {
                         } />
 
                     </View>
-                    <View style={styles.titles}>
-                        <Text style={styles.TitleDestaque}> Destaques </Text>
+                    <View >
+                        <Text style={{
+                            fontSize: 25,
+                            fontWeight: 'bold',
+                            padding: 10,
+                        }}> Destaques </Text>
                     </View>
-                    <ScrollView horizontal showsHorizontalScrollIndicator={false} >
 
-                        {cards.map((item) => (
-                            <TouchableOpacity style={styles.cards} onPress={() => navigation.navigate('locais', { item: item })}>
+                    <ScrollView horizontal showsHorizontalScrollIndicator={false}  >
+                        {cards.map((item, index) => (
+                            <TouchableOpacity style={{
+                                backgroundColor: 'gray',
+                                margin: 10,
+                                width: 300,
+                                height: 200,
+                                borderRadius: 20,
+                                overflow: 'hidden',
+
+                            }} onPress={() => navigation.navigate('locais', { item: item })}>
                                 <Image source={item.image} style={styles.imgcard} />
                                 <View style={styles.topText}>
                                     <Text style={styles.h2}>{item.titulo}</Text>
@@ -114,7 +132,53 @@ export default function Home({ navigation }) {
                             </TouchableOpacity>
                         ))}
                     </ScrollView>
-                </View>
+
+                    <View>
+                        <Text style={{
+                            fontSize: 25,
+                            fontWeight: 'bold',
+                            padding: 10,
+                        }}> Mais lugares </Text>
+                    </View>
+                    <View style={{ paddingHorizontal: 16 }}>
+                        <FlatList
+                            data={cards}
+                            renderItem={({ item }) => (
+
+                                <TouchableOpacity style={{
+                                    backgroundColor: '#FFF',
+                                    borderRadius: 16,
+                                    marginVertical: 16,
+                                    alignItems: 'center',
+                                    padding: 10,
+                                    overflow: 'hidden',
+
+                                    shadowColor: '#000',
+                                    shadowOffset: { width: 0, height: 10, },
+                                    shadowOpacity: 5,
+                                    shadowRadius: 8,
+                                }} onPress={() => navigation.navigate('locais', { item: item })}>
+
+
+                                    <Image style={{ width: 150, height: 150, resizeMode: 'cover', borderRadius: 16, }} source={item.image} />
+
+                                    <Text style={{
+                                        margin: 10,
+                                        fontWeight: 700,
+
+                                    }}>{item.titulo}</Text>
+                                    <Text style={{ width: 150, textAlign: 'justify', }}>{item.previa}</Text>
+
+                                </TouchableOpacity>
+                            )} numColumns={2}
+                            columnWrapperStyle={{
+                                justifyContent: 'space-between'
+                            }}
+                        />
+
+
+                    </View>
+                </View>{/*container fim*/}
             </ScrollView >
 
         </>
@@ -139,11 +203,6 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: '#000000ae'
     },
-    TitleDestaque: {
-        fontSize: 25,
-        fontWeight: 'bold',
-        padding: 10,
-    },
     h2: {
         color: '#FFF',
         fontSize: 25,
@@ -165,10 +224,6 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.5,
         shadowRadius: 7,
     },
-    titles: {
-        display: 'flex',
-        alignItems: 'center'
-    },
     CardList: {
         display: 'flex',
     },
@@ -182,12 +237,7 @@ const styles = StyleSheet.create({
         shadowRadius: 7,
     },
     cards: {
-        backgroundColor: 'gray',
-        margin: 10,
-        width: 300,
-        height: 200,
-        borderRadius: 20,
-        overflow: 'hidden',
+
     },
     imgcard: {
         width: '100%',
